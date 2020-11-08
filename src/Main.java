7eeaaf06e430f79ff1843b20eefea0489acdbb34
Sofia <ps.vargas@uniandes.edu.co>
2020-11-08 14:22:57 -0500

    Arreglo sigue null pointer

--- src/Main.java
+++ src/Main.java
@@ -1,232 +1,115 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main 
 {
-	private static Metodos metds;
 	public static String MD5 = "MD5";
 	public static String SHA256 = "SHA-256";
 	public static String SHA384 = "SHA-384";
 	public static String SHA512 = "SHA-512";
-	public static String[] alfabeto = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"};
-
+	
 	public static void main(String[] args) throws IOException 
 	{
-		//		try (   InputStreamReader is= new InputStreamReader(System.in); BufferedReader br = new BufferedReader(is);)
-		//		{
-		//			String msm = null;
-		//			String algoritmo = null;
-		//			boolean b=false;
-		//
-		//			String line = br.readLine();
-		//			while(!b)
-		//			{
-		//				System.out.println("Ingrese el mensaje seguido de el algoritmo (MD5, SHA256, SHA384 o SHA512) separado por ',' Ejemplo: holahola,MD5 ");
-		//				String[] linea = line.split(",");
-		//				msm=linea[0];
-		//				algoritmo=linea[1];
-		//				
-		//				if(msm!=null && algoritmo!=null && msm.length()<=7)
-		//				{
-		//					if(algoritmo.equalsIgnoreCase("MD5"))
-		//					{System.out.println(metds.generarCodigo(msm, MD5)); b=true;}
-		//					else if(algoritmo.equalsIgnoreCase("SHA256"))
-		//					{metds.generarCodigo(msm, SHA256); b=true;}
-		//					else if(algoritmo.equalsIgnoreCase("SHA384"))
-		//					{metds.generarCodigo(msm, SHA384); b=true;}
-		//					else if(algoritmo.equalsIgnoreCase("SHA512"))
-		//					{metds.generarCodigo(msm, SHA512); b=true;}
-		//				}	
-		//				
-		//				line = br.readLine();
-		//			}
-		//		}
-
-		boolean b=false;
+		boolean b = false;
 		String mensaje = null;
 		String algoritmo = null;
+		String codigo = null;
 		Scanner consola = new Scanner(System.in);
-		while(b==false)
-		{
-			System.out.println("Ingrese el mensaje seguido de el algoritmo (MD5, SHA256, SHA384 o SHA512) separado por ',' Ejemplo: holahola,MD5 ");
-			String linea = consola.nextLine();
-			String[] line = linea.split(",");
-			mensaje = line[0];
-			algoritmo = line[1];
-			String hash=null;
 
-			if(mensaje!=null && algoritmo!=null && mensaje.length()<=7)
+		while(b == false)
+		{
+			System.out.println("Caso 2 - Infraestructura Computacional");
+			System.out.println("1. Generar código criptográfico de hash");
+			System.out.println("2. Encontrar cadena");
+
+			String opcion = consola.nextLine();
+			if(opcion=="1")
 			{
-				if(algoritmo.equalsIgnoreCase("MD5"))
+				System.out.println("Ingrese el mensaje seguido de el algoritmo (MD5, SHA256, SHA384 o SHA512) separado por ',' Ejemplo: holahola,MD5 ");
+				String linea = consola.nextLine();
+				String[] line = linea.split(",");
+				mensaje = line[0].toLowerCase();
+				algoritmo = line[1];
+				String hash=null;
+
+				if(mensaje!=null && algoritmo!=null && mensaje.length()<=7)
 				{
-					hash = metds.generarCodigo(mensaje, MD5); 
-					identificarTam(mensaje, MD5);
-					b=true;
-				}
-				else if(algoritmo.equalsIgnoreCase("SHA256"))
-				{
-					hash = metds.generarCodigo(mensaje, SHA256);
-					identificarTam(mensaje, SHA256);
-					b=true;
-				}
-				else if(algoritmo.equalsIgnoreCase("SHA384"))
-				{
-					hash = metds.generarCodigo(mensaje, SHA384);
-					identificarTam(mensaje, SHA384);
-					b=true;
-				}
-				else if(algoritmo.equalsIgnoreCase("SHA512"))
-				{
-					hash = metds.generarCodigo(mensaje, SHA512);
-					identificarTam(mensaje, SHA512);
-					b=true;
+					if(algoritmo.equalsIgnoreCase("MD5"))
+					{
+						hash = Metodos.generarCodigo(mensaje, MD5); 
+						b=true;
+					}
+					else if(algoritmo.equalsIgnoreCase("SHA256"))
+					{
+						hash = Metodos.generarCodigo(mensaje, SHA256);
+						b=true;
+					}
+					else if(algoritmo.equalsIgnoreCase("SHA384"))
+					{
+						hash = Metodos.generarCodigo(mensaje, SHA384);
+						b=true;
+					}
+					else if(algoritmo.equalsIgnoreCase("SHA512"))
+					{
+						hash = Metodos.generarCodigo(mensaje, SHA512);
+						b=true;
+					}
+					else
+						System.out.println("El algoritmo no es correcto");
 				}
 				else
-					System.out.println("El algoritmo no es correcto");
+					System.out.println("Hubo algún problema");
+
+				System.out.println("Código criptográfico de hash: " + hash);
+
+
 			}
 			else
-				System.out.println("Hubo algún problema");
-
-			System.out.println("Código criptográfico de hash: "+hash);
-			
-			String cadena = metds.identificar_entrada(hash, algoritmo);
-			
-			if(cadena!=null)
-				System.out.println("La cadena que se usó para generar el código fue: " + cadena );
-			else 
-				System.out.println("No se encontró respuesta");
-
-		}
-	}
-
-	public static void identificarTam(String msm, String alg) throws IOException
-	{
-		int n = msm.length();
-		if(n==1)
-		{
-			FileWriter fw = new FileWriter("data/diccionario.txt");
-			PrintWriter pw = new PrintWriter(fw);
-
-			for(int i=0; i<alfabeto.length; i++)
 			{
-				String act = alfabeto[i];
-				pw.println(act + "," + metds.generarCodigo(act,alg));
-			}
-			fw.close();
-		}
-		else if(n==2) crearDiccionario(lista2(),alg);
-		else if(n==3) crearDiccionario(lista3(),alg);
-		else if(n==4) crearDiccionario(lista4(),alg);
-		else if(n==5) crearDiccionario(lista5(),alg);
-		else if(n==6) crearDiccionario(lista6(),alg);
-		else crearDiccionario(lista7(),alg);
+				System.out.println("Ingrese el código y el algoritmo (MD5, SHA256, SHA384 o SHA512) separado por ',' Ejemplo: a3dk203,MD5");
+				String linea = consola.nextLine();
+				String[] line = linea.split(",");
+				codigo = line[0];
+				algoritmo = line[1];
+				String cadena = null;
+				if(codigo!=null && algoritmo!=null)
+				{
+					if(algoritmo.equalsIgnoreCase("MD5"))
+					{
+						cadena = Metodos.identificar_entrada(codigo, MD5);
+						b=true;
+					}
+					else if(algoritmo.equalsIgnoreCase("SHA256"))
+					{
+						cadena = Metodos.identificar_entrada(codigo, SHA256);
+						b=true;
+					}
+					else if(algoritmo.equalsIgnoreCase("SHA384"))
+					{
+						cadena = Metodos.identificar_entrada(codigo, SHA384);
+						b=true;
+					}
+					else if(algoritmo.equalsIgnoreCase("SHA512"))
+					{
+						cadena = Metodos.identificar_entrada(codigo, SHA512);
+						b=true;
+					}
+					else
+						System.out.println("El algoritmo no es correcto");
+				}
+				else 
+					System.out.println("Hubo algún problema");
 
-	}
-	public static void crearDiccionario(ArrayList<String> array, String alg) throws IOException
-	{
-		FileWriter fw = new FileWriter("data/Diccionario.txt");
-		PrintWriter pw = new PrintWriter(fw);
-
-		for(int i=0; i<array.size(); i++)
-		{
-			String act = array.get(i);
-			pw.println(act + "," + metds.generarCodigo(act,alg));
-		}
-		fw.close();
-	}
-
-	public static ArrayList<String> lista2() throws IOException
-	{
-		ArrayList<String> array = new ArrayList<String>();
-
-		for(int i=0; i<alfabeto.length; i++)
-		{
-			for(int j=0; j<alfabeto.length; j++)
-			{
-				array.add(alfabeto[i]+alfabeto[j]);
+				if(cadena!=null)
+					System.out.println("La cadena que se usó para generar el código fue: " + cadena );
+				else 
+					System.out.println("No se encontró respuesta");	
 			}
 		}
-		return array;
+		consola.close();
 	}
 
-	public static ArrayList<String> lista3() throws IOException
-	{
-		ArrayList<String> array = new ArrayList<String>();
-		ArrayList<String> anterior = lista2();
-
-		for(int i=0; i<anterior.size(); i++)
-		{
-			for(int j=0; j<alfabeto.length; j++)
-			{
-				array.add(anterior.get(i)+alfabeto[j]);
-			}
-		}
-		return array;
-	}
-	public static ArrayList<String> lista4() throws IOException
-	{
-		ArrayList<String> array = new ArrayList<String>();
-		ArrayList<String> anterior = lista3();
-
-		for(int i=0; i<anterior.size(); i++)
-		{
-			for(int j=0; j<alfabeto.length; j++)
-			{
-				array.add(anterior.get(i)+alfabeto[j]);
-			}
-		}
-		return array;
-	}
-	public static ArrayList<String> lista5() throws IOException
-	{
-		ArrayList<String> array = new ArrayList<String>();
-		ArrayList<String> anterior = lista4();
-
-		for(int i=0; i<anterior.size(); i++)
-		{
-			for(int j=0; j<alfabeto.length; j++)
-			{
-				array.add(anterior.get(i)+alfabeto[j]);
-			}
-		}
-		return array;
-	}
-	public static ArrayList<String> lista6() throws IOException
-	{
-		ArrayList<String> array = new ArrayList<String>();
-		ArrayList<String> anterior = lista5();
-
-		for(int i=0; i<anterior.size(); i++)
-		{
-			for(int j=0; j<alfabeto.length; j++)
-			{
-				array.add(anterior.get(i)+alfabeto[j]);
-			}
-		}
-		return array;
-	}
-	public static ArrayList<String> lista7() throws IOException
-	{
-		ArrayList<String> array = new ArrayList<String>();
-		ArrayList<String> anterior = lista6();
-
-		for(int i=0; i<anterior.size(); i++)
-		{
-			for(int j=0; j<alfabeto.length; j++)
-			{
-				array.add(anterior.get(i)+alfabeto[j]);
-			}
-		}
-		return array;
-	}
+	
 }
 
 
--- src/Metodos.java
+++ src/Metodos.java
@@ -1,39 +1,23 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.security.*;
+import java.io.PrintWriter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 public class Metodos 
 {
-
+	public static String[] alfabeto = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"};
+	public static String[] diccionarios = new String[7];
 
 	/***
-	 *Convierte un arreglo de bytes a String usando valores hexadecimales
-	 * @param digest arreglo de bytes a convertir
-	 *@return String creado a partir de digest ***/
-
-	private static String toHexadecimal(byte[] digest)
-	{ 
-		String hash = "";
-		for(byte aux : digest)
-		{
-			int b = aux & 0xff;
-			if(Integer.toHexString(b).length() == 1)
-			{ hash += "0";}
-			hash += Integer.toHexString(b);
-		}
-		return hash;
-	}
-	/***
-	 * Encripta un mensaje de texto mediante algoritmo de resumen de mensaje.
+	 * Encripta un mensaje de texto.
 	 * @param message texto a encriptar
 	 * @param algorithm algoritmo de encriptacion, puede ser: MD5, SHA-256, SHA-384, SHA-512
 	 * @return mensaje encriptado ***/
-	public String generarCodigo(String msm, String algoritmo)
+	public static String generarCodigo(String msm, String algoritmo)
 	{
 		byte[] digest = null;
 		byte[] buffer = msm.getBytes();
@@ -51,24 +35,170 @@ public class Metodos
 		}
 
 	}
-
-	public String identificar_entrada(String codigo, String algoritmo) throws IOException
-	{
-		BufferedReader bf = new BufferedReader(new FileReader("data/diccionario.txt"));
-		String c = bf.readLine();
-		String  cadena = null;
-		boolean encontro = false;
-		while (cadena!=null && !encontro) 
+	/***
+	 *Convierte un arreglo de bytes a String usando valores hexadecimales
+	 * @param digest arreglo de bytes a convertir
+	 *@return String creado a partir de digest ***/
+	private static String toHexadecimal(byte[] digest)
+	{ 
+		String hash = "";
+		for(byte aux : digest)
 		{
-			String[] cad = cadena.split(",");
-			if(codigo.equals(cad[1]))
+			int b = aux & 0xff;
+			if(Integer.toHexString(b).length() == 1)
+			{ hash += "0";}
+			hash += Integer.toHexString(b);
+		}
+		return hash;
+	}
+
+	public static String identificar_entrada(String codigo, String algoritmo) throws IOException
+	{
+		boolean encontro = false;
+		String  cadena = null;
+		crearArchivos(algoritmo);
+		
+		for(int i=0; i<diccionarios.length && !encontro; i++)
+		{
+			BufferedReader bf = new BufferedReader(new FileReader(diccionarios[i]));
+			String c = bf.readLine();
+			
+			while (c!=null && !encontro) 
 			{
-				encontro = true;
-				cadena = cad[0];	
+				String[] cad = c.split(",");
+				if(codigo.equals(cad[1]))
+				{
+					encontro = true;
+					cadena = cad[0];	
+				}
 			}
 		}
-
 		return cadena;
+	}
+	
+	public static void crearArchivos(String alg) throws IOException 
+	{
+		String ruta = "data/diccionario1.txt";
+		FileWriter fw = new FileWriter(ruta);
+		PrintWriter pw = new PrintWriter(fw);
+		diccionarios[0] = ruta;
+
+		for(int i=0; i<alfabeto.length; i++)
+		{
+			String act = alfabeto[i];
+			pw.println(act + "," + generarCodigo(act,alg));
+		}
+		fw.close();
+
+		crearDiccionarios(lista2(),alg, 2);
+		crearDiccionarios(lista3(),alg, 3);
+		crearDiccionarios(lista4(),alg, 4);
+		crearDiccionarios(lista5(),alg, 5);
+		crearDiccionarios(lista6(),alg, 6);
+		crearDiccionarios(lista7(),alg, 7);
+
+
+	}
+
+	public static void crearDiccionarios(ArrayList<String> array, String alg, int n) throws IOException 
+	{
+		String ruta = "data/diccionario"+ n +".txt";
+		FileWriter fw = new FileWriter(ruta);
+		PrintWriter pw = new PrintWriter(fw);
+		diccionarios[n-1]=ruta;
+
+		for(int i=0; i<array.size(); i++)
+		{
+			String act = array.get(i);
+			pw.println(act + "," + generarCodigo(act,alg));
+		}
+		fw.close();
+
+	}
+
+	public static ArrayList<String> lista2() throws IOException
+	{
+		ArrayList<String> array = new ArrayList<String>();
+
+		for(int i=0; i<alfabeto.length; i++)
+		{
+			for(int j=0; j<alfabeto.length; j++)
+			{
+				array.add(alfabeto[i]+alfabeto[j]);
+			}
+		}
+		return array;
+	}
+
+	public static ArrayList<String> lista3() throws IOException
+	{
+		ArrayList<String> array = new ArrayList<String>();
+		ArrayList<String> anterior = lista2();
+
+		for(int i=0; i<anterior.size(); i++)
+		{
+			for(int j=0; j<alfabeto.length; j++)
+			{
+				array.add(anterior.get(i)+alfabeto[j]);
+			}
+		}
+		return array;
+	}
+	public static ArrayList<String> lista4() throws IOException
+	{
+		ArrayList<String> array = new ArrayList<String>();
+		ArrayList<String> anterior = lista3();
+
+		for(int i=0; i<anterior.size(); i++)
+		{
+			for(int j=0; j<alfabeto.length; j++)
+			{
+				array.add(anterior.get(i)+alfabeto[j]);
+			}
+		}
+		return array;
+	}
+	public static ArrayList<String> lista5() throws IOException
+	{
+		ArrayList<String> array = new ArrayList<String>();
+		ArrayList<String> anterior = lista4();
+
+		for(int i=0; i<anterior.size(); i++)
+		{
+			for(int j=0; j<alfabeto.length; j++)
+			{
+				array.add(anterior.get(i)+alfabeto[j]);
+			}
+		}
+		return array;
+	}
+	public static ArrayList<String> lista6() throws IOException
+	{
+		ArrayList<String> array = new ArrayList<String>();
+		ArrayList<String> anterior = lista5();
+
+		for(int i=0; i<anterior.size(); i++)
+		{
+			for(int j=0; j<alfabeto.length; j++)
+			{
+				array.add(anterior.get(i)+alfabeto[j]);
+			}
+		}
+		return array;
+	}
+	public static ArrayList<String> lista7() throws IOException
+	{
+		ArrayList<String> array = new ArrayList<String>();
+		ArrayList<String> anterior = lista6();
+
+		for(int i=0; i<anterior.size(); i++)
+		{
+			for(int j=0; j<alfabeto.length; j++)
+			{
+				array.add(anterior.get(i)+alfabeto[j]);
+			}
+		}
+		return array;
 	}
 
 }
